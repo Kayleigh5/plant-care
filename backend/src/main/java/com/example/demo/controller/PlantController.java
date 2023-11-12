4c9a1ff0262e3812be744b788db4189e2954c645
Kayleigh Beard <13117481+Kayleigh5@users.noreply.github.com>
2023-11-12 15:07:47 +0100

    Feature: remove all plants

--- backend/src/main/java/com/example/demo/controller/PlantController.java
+++ backend/src/main/java/com/example/demo/controller/PlantController.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -37,13 +38,21 @@ public class PlantController {
 			if (plants.isEmpty()) {
 				return new ResponseEntity<>(HttpStatus.NO_CONTENT);
 			}
-
-			System.out.println(plants);
 			return new ResponseEntity<>(plants, HttpStatus.OK);
 		} catch (Exception e) {
-			System.out.println(e);
 			return new ResponseEntity<>(null, HttpStatus.INTERNAL_SERVER_ERROR);
 		}
+	}
+
+	@DeleteMapping("/plants")
+	public ResponseEntity<HttpStatus> deleteAllPlants() {
+		try {
+			plantRepository.deleteAll();
+			return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+		} catch (Exception e) {
+			return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+		}
+
 	}
     
 }
